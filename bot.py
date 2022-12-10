3bc4fc0710e71ee67a9ae21891f1c9e5c07e554c
Vinicius Sena da Silva Amarante <63321807+O-Braixen@users.noreply.github.com>
2022-12-10 10:21:04 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ bot = commands.Bot('.')
 bot.remove_command('help')
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 300','options': '-vn'}
 
-# Podešavanja:
-Voice_Soba_Id = 123456789 # Ovdije upišete ID od voice sobe gdije želite da bot bude.
-Link_Radio_Stanice = 'http://balkan.dj.topstream.net:8070/;' #Ovdije upišete link online radio stanice.
+# Definições:
+Voice_Soba_Id = 1049168326586286100 # Aqui você insere o ID da sala de voz onde deseja que o bot esteja.
+Link_Radio_Stanice = 'http://192.168.2.13:5006/listen/braixens_radio/radio.mp3' #Oentre no link da estação de rádio online aqui.
 
 @bot.event
 async def on_ready():
@@ -16,5 +16,6 @@ async def on_ready():
     vc = await bot.get_channel(Voice_Soba_Id).connect()
     vc.play(LinkRadia)
     print(bot.user)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"Braixen's Radio - {['title']}")) #atualiza o status do bot
 
-bot.run('TOKEN') #Vaš token
+bot.run('seu_lindo_Token_Aqui') #seu token
